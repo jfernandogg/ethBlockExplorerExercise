d46e66bdbdc2b1d2c8b5a532d0e3b7aca83b88b8
Juan Fernando Gallego Gomez <juan.gallego@gmail.com>
2023-09-18 13:02:35 -0500

    Block details and block number field functionality added

--- src/App.js
+++ src/App.js
@@ -21,16 +21,67 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockDetails, setBlockDetails] = useState({});
+  const [inputValue, setInputValue] = useState('');
+
+  const fetchBlockData = async (number) => {
+    let blockNum = number || await alchemy.core.getBlockNumber();
+    setBlockNumber(blockNum);
+
+    // Get block details
+    const blockData = await alchemy.core.getBlock(blockNum);
+    setBlockDetails(blockData);
+  };
 
   useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+    fetchBlockData();
+  }, []);  // The empty dependency array ensures this effect runs once when the component mounts.
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (inputValue) {
+      await fetchBlockData(inputValue);
     }
+  };
 
-    getBlockNumber();
-  });
+  return (
+    <div className="App container mt-5" style={{ maxWidth: '70%', marginLeft: '15%', marginRight: '15%' }}>
+      <form onSubmit={handleSubmit} className="mb-3">
+        <input
+          type="text"
+          placeholder="Enter block number"
+          className="form-control"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </form>
+      <div className="mb-3">Block Number: {blockNumber}</div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th style={{ verticalAlign: 'top' }}>Field</th>
+            <th style={{ verticalAlign: 'top' }}>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(blockDetails).map(([key, value]) => {
+            if (key === 'transactions' && Array.isArray(value)) {
+              value = value.join('<br/>\n');
+            } else if (typeof value === 'object') {
+              value = JSON.stringify(value);
+            }
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+            return (
+              <tr key={key}>
+                <td>{key}</td>
+                <td dangerouslySetInnerHTML={{ __html: value }}></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
